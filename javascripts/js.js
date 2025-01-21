534857fbb6c6340bd427d83cd31a5f68970df1a7
sonyaorlovav <svorlova@edu.hse.ru>
2025-01-21 09:14:07 +0300

    Add files via upload

---
+++ javascripts/js.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+// alert('Привет');
+// alert('мир');
+// alert('Привет мир');
+
+// let x= 10;
+// let y= 10;
+// alert(x+y)
+
+// alert(1/0);
+// alert("не число"/2);
+// NaN, null, undefined
+
+// let str = "Привет";
+// let str2 = 'Одинарные кавычки тоже подойдут';
+// let phrase = `Обратные кавычки позволяют встраивать переменные ${str}`;
+
+// let str = "Привет";
+// alert(`Обратные кавычки позволяют встраивать переменные ${str}`);
+
+// let ter = 4 > 1;
+
+//ввод чисел
+let a = parseInt (prompt("Введите первое число:"));
+let b = parseInt (prompt("Введите второе число:"));
+
+// Проверка условия
+console.log (true)(a > 0 || b > 0); 
+console.log (false)(a < 0 || b < 0); 
+
+
+
+
+
+})
